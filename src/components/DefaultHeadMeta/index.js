7d543953ddd3095f39b4d592d6b67158328983a4
Ray Gesualdo <ray@rjgesualdo.com>
2017-01-02 14:24:10 -0500

    fix #25

--- src/components/DefaultHeadMeta/index.js
+++ src/components/DefaultHeadMeta/index.js
@@ -14,7 +14,7 @@ const DefaultHeadMeta = (props, {metadata: {pkg}}) => {
     link: [
       // { rel: "stylesheet", href: "https://cdnjs.cloudflare.com/ajax/libs/meyer-reset/2.0/reset.min.css", integrity: "sha256-gvEnj2axkqIj4wbYhPjbWV7zttgpzBVEgHub9AAZQD4=", crossorigin: "anonymous" },
       // { rel: "stylesheet", href: "https://cdnjs.cloudflare.com/ajax/libs/normalize/5.0.0/normalize.min.css", integrity: "sha256-t2/7smZfgrST4FS1DT0bs/KotCM74XlcqZN5Vu7xlrw=", crossorigin: "anonymous" },
-      { rel: 'stylesheet', href: 'http://fonts.googleapis.com/css?family=Arvo:400,700' },
+      { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css?family=Arvo:400,700' },
       { rel: 'stylesheet', href: 'https://cdnjs.cloudflare.com/ajax/libs/highlight.js/9.9.0/styles/atom-one-dark.min.css' }
     ],
     script: [
