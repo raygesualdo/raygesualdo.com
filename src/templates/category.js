f80c226e2d55d81299a0e41606d6d59c9ecc57dd
Ray Gesualdo <ray@raygesualdo.com>
2019-03-05 22:15:48 -0500

    refactored queries getting categories

--- src/templates/category.js
+++ src/templates/category.js
@@ -1,6 +1,5 @@
 import React from 'react'
 import { graphql } from 'gatsby'
-import get from 'lodash/get'
 import Helmet from 'react-helmet'
 import Layout from '../components/Layout/Layout'
 import PageTitle from '../components/PageTitle/PageTitle'
@@ -8,16 +7,12 @@ import PageLayout from '../components/PageLayout/PageLayout'
 import ArticleBlock from '../components/ArticleBlock/ArticleBlock'
 
 const CategoryTemplate = props => {
-  const posts = get(props, 'data.posts.edges', []).filter(
-    ({ node }) =>
-      get(node, 'frontmatter.category.slug') === props.data.category.slug
-  )
   return (
     <Layout>
       <Helmet title={`Category: ${props.data.category.name}`} />
       <PageTitle title={`Category: ${props.data.category.name}`} />
       <PageLayout>
-        {posts.map(({ node: post }) => (
+        {props.data.posts.nodes.map(post => (
           <ArticleBlock
             key={post.fields.slug}
             post={post}
@@ -33,34 +28,32 @@ export default CategoryTemplate
 
 export const pageQuery = graphql`
   query($slug: String!) {
-    category: categoriesYaml(fields: { slug: { eq: $slug } }) {
+    category: categoriesYaml(slug: { eq: $slug }) {
       slug
       name
     }
     posts: allMarkdownRemark(
-      filter: { fields: { contentGroup: { eq: "posts" } } }
+      filter: { frontmatter: { category: { eq: $slug } } }
       sort: { fields: frontmatter___date, order: DESC }
     ) {
-      edges {
-        node {
-          frontmatter {
-            title
-            date(formatString: "YYYY-MM-DD")
-            category {
-              slug
-              name
-            }
-          }
-          fields {
+      nodes {
+        frontmatter {
+          title
+          date(formatString: "YYYY-MM-DD")
+          category: categoryData {
+            name
             slug
           }
-          excerpt
-          timeToRead
-          wordCount {
-            paragraphs
-            sentences
-            words
-          }
+        }
+        fields {
+          slug
+        }
+        excerpt
+        timeToRead
+        wordCount {
+          paragraphs
+          sentences
+          words
         }
       }
     }
