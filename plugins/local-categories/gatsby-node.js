f7327b3983a514ae94f670358cb18be537b3f09e
Ray Gesualdo <ray@raygesualdo.com>
2019-04-14 16:44:14 -0400

    created local categories plugin

--- plugins/local-categories/gatsby-node.js
+++ plugins/local-categories/gatsby-node.js
@@ -1,3 +1,8 @@
+const { merge } = require('lodash')
+const {
+  findManyPaginated,
+} = require('../../node_modules/gatsby/dist/schema/resolvers.js')
+
 exports.createResolvers = ({ createResolvers }) => {
   createResolvers({
     MarkdownRemarkFrontmatter: {
@@ -8,6 +13,29 @@ exports.createResolvers = ({ createResolvers }) => {
             type: 'CategoriesYaml',
             query: { filter: { slug: { eq: source.category } } },
             firstOnly: true,
+          })
+          return data
+        },
+      },
+    },
+    CategoriesYaml: {
+      posts: {
+        type: 'MarkdownRemarkConnection',
+        args: {
+          filter: 'MarkdownRemarkFilterInput',
+          sort: 'MarkdownRemarkSortInput',
+          skip: 'Int',
+          limit: 'Int',
+        },
+        async resolve(source, args, context, info) {
+          merge(args, {
+            filter: { frontmatter: { category: { eq: source.slug } } },
+          })
+          const typeName = 'MarkdownRemark'
+          const data = await findManyPaginated(typeName)({
+            args,
+            context,
+            info,
           })
           return data
         },
