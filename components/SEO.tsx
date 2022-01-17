357fc91f4be07d77f16f8e79032fd62fde8648c5
Ray Gesualdo <ray@raygesualdo.com>
2022-01-17 16:25:16 -0500

    add social media images

---
+++ components/SEO.tsx
@@ -0,0 +1,28 @@
+import Head from 'next/head'
+import { FacebookOpenGraph as FOG } from '@resoc/core'
+import { PostData } from '../lib/posts'
+import { SocialImages } from '../lib/socialImages'
+
+export const SEO = ({
+  post,
+  socialImages,
+}: {
+  post: PostData
+  socialImages: SocialImages
+}) => {
+  return (
+    <Head>
+      <meta name="description" content={post.excerpt} />
+      <meta name="twitter:card" content="summary_large_image" />
+      <meta name="twitter:site" content="@raygesualdo" />
+      <meta name="twitter:title" content={post.title} />
+      <meta name="twitter:description" content={post.excerpt} />
+      <meta name="twitter:image" content={socialImages.twitterImage} />
+      <meta property="og:title" content={post.title} />
+      <meta property="og:description" content={post.excerpt} />
+      <meta property="og:image" content={socialImages.ogImage} />
+      <meta property="og:image:width" content={String(FOG.width)} />
+      <meta property="og:image:height" content={String(FOG.height)} />
+    </Head>
+  )
+}
