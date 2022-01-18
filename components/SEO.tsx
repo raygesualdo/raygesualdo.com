f59eeeef9363c2758da34e098ca14ce92f3ed268
Ray Gesualdo <ray@raygesualdo.com>
2022-01-18 02:08:57 -0500

    convert social image links to absolute URLs

--- components/SEO.tsx
+++ components/SEO.tsx
@@ -2,6 +2,9 @@ import Head from 'next/head'
 import { FacebookOpenGraph as FOG } from '@resoc/core'
 import { PostData } from '../lib/posts'
 import { SocialImages } from '../lib/socialImages'
+import { config } from '../lib/config'
+
+const abs = (url: string) => `${config.siteUrl}${url}`
 
 export const SEO = ({
   post,
@@ -17,10 +20,10 @@ export const SEO = ({
       <meta name="twitter:site" content="@raygesualdo" />
       <meta name="twitter:title" content={post.title} />
       <meta name="twitter:description" content={post.excerpt} />
-      <meta name="twitter:image" content={socialImages.twitterImage} />
+      <meta name="twitter:image" content={abs(socialImages.twitterImage)} />
       <meta property="og:title" content={post.title} />
       <meta property="og:description" content={post.excerpt} />
-      <meta property="og:image" content={socialImages.ogImage} />
+      <meta property="og:image" content={abs(socialImages.ogImage)} />
       <meta property="og:image:width" content={String(FOG.width)} />
       <meta property="og:image:height" content={String(FOG.height)} />
     </Head>
