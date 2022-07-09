c057081df72c6dca06537803a6f13d8e2bac8c8e
Ray Gesualdo <ray@raygesualdo.com>
2022-07-08 21:59:04 -0400

    attempt to fix twitter previews

--- components/SEO.tsx
+++ components/SEO.tsx
@@ -17,10 +17,12 @@ export const SEO = ({
     <Head>
       <meta name="description" content={post.excerpt} />
       <meta name="twitter:card" content="summary_large_image" />
+      <meta name="twitter:creator" content="@raygesualdo" />
       <meta name="twitter:site" content="@raygesualdo" />
       <meta name="twitter:title" content={post.title} />
       <meta name="twitter:description" content={post.excerpt} />
       <meta name="twitter:image" content={abs(socialImages.twitterImage)} />
+      <meta name="twitter:image:src" content={abs(socialImages.twitterImage)} />
       <meta property="og:title" content={post.title} />
       <meta property="og:description" content={post.excerpt} />
       <meta property="og:image" content={abs(socialImages.ogImage)} />
