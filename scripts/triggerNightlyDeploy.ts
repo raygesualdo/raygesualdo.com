1354d7fe11231fce6076209ea6e5687c09c628ec
Ray Gesualdo <ray.gesualdo@salesloft.com>
2022-11-16 20:39:55 -0500

    refactored nightly deploy trigger using Node.js v18 features

--- scripts/triggerNightlyDeploy.ts
+++ scripts/triggerNightlyDeploy.ts
@@ -1,20 +1,14 @@
-// @ts-expect-error
-import fetch from 'node-fetch'
 import { getAllPosts } from '../lib/posts'
 
 const CURRENT_DATE = new Date().toISOString().slice(0, 10)
+const posts = await getAllPosts({ includeDrafts: true })
 
-async function main() {
-  const posts = await getAllPosts({ includeDrafts: true })
-  if (
-    posts.some((post) => post.date === CURRENT_DATE) &&
-    process.env.NETLIFY_BUILD_HOOK_URL
-  ) {
-    console.log(
-      'A post scheduled for today was found. Triggering a Netlify build.'
-    )
-    fetch(process.env.NETLIFY_BUILD_HOOK_URL, { method: 'POST' })
-  }
+if (
+  posts.some((post) => post.date === CURRENT_DATE) &&
+  process.env.NETLIFY_BUILD_HOOK_URL
+) {
+  console.log(
+    'A post scheduled for today was found. Triggering a Netlify build.'
+  )
+  await fetch(process.env.NETLIFY_BUILD_HOOK_URL, { method: 'POST' })
 }
-
-main()
