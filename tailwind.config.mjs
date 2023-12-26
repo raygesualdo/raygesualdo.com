147ca118a80121a8a9750e8a642cc6df40880948
Ray Gesualdo <ray.gesualdo@salesloft.com>
2023-12-26 15:43:10 -0500

    migrate layout from old site

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
+  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  darkMode: 'class',
+  theme: {
+    fontFamily: {
+      display: 'Montserrat, sans-serif',
+      body: 'Georgia, Cambria, serif',
+    },
+    extend: {},
+  },
+  plugins: [],
 }
