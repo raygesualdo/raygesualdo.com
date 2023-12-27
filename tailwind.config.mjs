fff60ed0ed3f73e912eb7c21b52c7962c9cf99c8
Ray Gesualdo <ray.gesualdo@salesloft.com>
2023-12-26 21:10:21 -0500

    add about me page

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,3 +1,5 @@
+import typography from '@tailwindcss/typography'
+
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
@@ -7,7 +9,52 @@ export default {
       display: 'Montserrat, sans-serif',
       body: 'Georgia, Cambria, serif',
     },
-    extend: {},
+    extend: {
+      typography: (theme) => ({
+        lg: {
+          css: {
+            blockquote: {
+              paddingLeft: null,
+            },
+            pre: {
+              paddingTop: null,
+              paddingBottom: null,
+              paddingLeft: null,
+              paddingRight: null,
+            },
+          },
+        },
+        DEFAULT: {
+          css: {
+            color: null,
+            'h1, h2, h3, h4, h5, h6': {
+              color: theme('colors.red.500'),
+              fontFamily: theme('fontFamily.display'),
+            },
+            code: {
+              fontStyle: 'normal',
+              fontWeight: 'normal',
+            },
+            'code::before': {
+              content: 'none',
+            },
+            'code::after': {
+              content: 'none',
+            },
+            blockquote: null,
+            'blockquote p': {
+              margin: '0 !important',
+            },
+            'figure figcaption': {
+              color: null,
+            },
+            summary: {
+              cursor: 'pointer',
+            },
+          },
+        },
+      }),
+    },
   },
-  plugins: [],
+  plugins: [typography],
 }
